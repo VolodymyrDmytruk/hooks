9f298aab5a30e64e1c2df82cfe5a30ed9e79bd3b
VolodymyrDmytruk <dmytruk.vova@gmail.com>
2023-07-08 17:32:23 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { useState,  useEffect } from 'react';
 import './App.css';
 
 export default function App() {
+	const [inputValue, setInputValue] = useState();
+
+	useEffect(() => {
+		const storedData = localStorage.getItem('appData');
+		if (storedData) {
+			setInputValue(storedData);			
+		}
+	}, []);
+
+	const inputChange = (e) => {
+		const newValue = e.target.value;
+		 setInputValue(newValue);
+		 localStorage.setItem('appData', newValue);
+	 };
 
   return (
     <div>
-      React Marathon, appData: <input size='5'></input>
+      React Marathon, appData: <input size='5' value={inputValue} onChange={inputChange} type="text"/>
     </div>
   );
 }
